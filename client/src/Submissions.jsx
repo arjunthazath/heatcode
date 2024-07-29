b814fe3c973809847e7efe30bf314441c9f457b6
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2024-07-29 08:32:43 +0530

    don

--- client/src/Submissions.jsx
+++ client/src/Submissions.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 
 function Submissions() {
+    const { pid } = useParams();
+    const cleanId = pid.substring(1);
+    const [submissions, setSubmissions] = useState([]);
+
+    const init = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/submissions/${cleanId}`, {
+                method: "GET",
+            });
+            const json = await response.json();
+            setSubmissions(json.submissions);
+        } catch (error) {
+            console.error("Failed to fetch problems:", error);
+        }
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
   return (
-    <div>Submissions</div>
+    <div>{submissions}</div>
   )
 }
 
